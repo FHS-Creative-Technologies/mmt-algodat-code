765438606a0f2ec39c3d2ddd6fc3522e73f6a924
Andreas Bilke <andreas@bilke.org>
2023-03-03 10:08:39 +0100

    Jaro Winkler: Remove unused imports and add error handling for inputs

--- Algorithmen/JaroWinkler/JaroWinkler.cs
+++ Algorithmen/JaroWinkler/JaroWinkler.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace AlgoDat
 {
     public static class JaroWinkler
--- Algorithmen/JaroWinkler/Program.cs
+++ Algorithmen/JaroWinkler/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using AlgoDat;
 
-using AlgoDat;
+if (args.Length != 2)
+{
+    Console.WriteLine("Usage: dotnet run <string1> <string2>");
+}
 
 string s1 = args[0];
 string s2 = args[1];
