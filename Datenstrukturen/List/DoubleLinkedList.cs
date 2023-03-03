8d67fdf2cabba4910b3496e5483c65376928f66b
Andreas Bilke <andreas@bilke.org>
2023-03-03 09:04:43 +0100

    DoubleLinkedList: Remove unused imports and create XML doc

--- Datenstrukturen/List/DoubleLinkedList.cs
+++ Datenstrukturen/List/DoubleLinkedList.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace AlgoDat
 {
@@ -44,22 +42,25 @@ namespace AlgoDat
             _counter = 0;
         }
         
-        // -------Explanation keyword: yield-------
-        // yield can only be used to return an IEnumerator or and IEnumerable object.
-        // yield tells the compiler that the method it appears in is an "iteration block"
-        // In this case, we want to return the element of the list the Enumerator is
-        // currently "pointing at", as in: we iterate through our list
-        // with for each, the compiler needs to know what the element is we
-        // are currently looking at / working with.
-        // When just calling GetEnumerator() the code will
-        // ACTUALLY NOT EXECUTE AT ALL!
-        // It simply returns an IEnumerator<T> object and stops!
-        // When calling the MoveNext() method (which is implemented by the IEnumerable interface
-        // and called every time we move to the next element in a (f.ex.) for each),
-        // the code will execute, until it reaches the first yield. 
-        // It then simply returns the node.Key element - and stops executing this method.
-        // On the next call of MoveNext(), the execution takes off from where it left before -
-        // it will therefore keep going with line "node = node.Next;" until it reaches yield again.
+        /// <summary>Create an enumerator for the <cref="DoubleLinkedList"></summary>
+        /// <remarks>
+        /// yield can only be used to return an IEnumerator or and IEnumerable object.
+        /// yield tells the compiler that the method it appears in is an "iteration block"
+        /// In this case, we want to return the element of the list the Enumerator is
+        /// currently "pointing at", as in: we iterate through our list
+        /// with for each, the compiler needs to know what the element is we
+        /// are currently looking at / working with.
+        /// When just calling "M:AlgoDat:GetEnumerator()" the code will
+        /// ACTUALLY NOT EXECUTE AT ALL!
+        /// It simply returns an "C:System.Collections.Generic.IEnumerator<T>" object and stops!
+        /// When calling the MoveNext() method (which is implemented by the IEnumerable interface
+        /// and called every time we move to the next element in a (e.g.) for each),
+        /// the code will execute, until it reaches the first yield. 
+        /// It then simply returns the node.Key element - and stops executing this method.
+        /// On the next call of MoveNext(), the execution takes off from where it left before -
+        /// it will therefore keep going with line "node = node.Next;" until it reaches yield again.
+        /// </remarks>
+        /// <returns>An iterator over the double linked list</returns>
         public IEnumerator<T> GetEnumerator()
         {
             var node = Head;
--- Datenstrukturen/List/Program.cs
+++ Datenstrukturen/List/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using AlgoDat;
+﻿using AlgoDat;
 
 DoubleLinkedList<int> myList = new();
 
