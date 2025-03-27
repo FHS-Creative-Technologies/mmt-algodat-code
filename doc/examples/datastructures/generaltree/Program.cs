a6e4517a457e723aa41798b4d4f5d1e10b3ee26b
Andreas Bilke <andreas@bilke.org>
2025-03-27 16:19:04 +0100

    Adjust example code for general tree

--- doc/examples/datastructures/generaltree/Program.cs
+++ doc/examples/datastructures/generaltree/Program.cs
@@ -1,10 +1,10 @@
 ﻿using FHS.CT.AlgoDat.DataStructures;
 
 Tree<int> tree = new();
-Tree<int>.Node<int> root = new(5);
+Tree<int>.Node root = new(5);
 tree.Root = root;
 
-Tree<int>.Node<int> child1 = new(3);
+Tree<int>.Node child1 = new(3);
 root.Left = child1;
 
 tree.InorderTreeWalk(root);
